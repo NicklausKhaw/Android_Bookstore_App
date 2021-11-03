3142e11b62610e92e52ed64b47a6ba053fa30896
Nicklaus <nicklaus5352@gmail.com>
2021-11-03 15:43:13 +0800

    Modified

--- app/src/main/java/com/example/bookstoreapp/EditActivity.kt
+++ app/src/main/java/com/example/bookstoreapp/EditActivity.kt
@@ -1,20 +1,27 @@
 package com.example.bookstoreapp
 
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import com.example.bookstoreapp.data.DatabaseHandler
 import com.example.bookstoreapp.databinding.ActivityEditBinding
 import com.example.bookstoreapp.model.BookModelClass
@@ -35,8 +42,11 @@ class EditActivity : AppCompatActivity() {
         setContentView(binding.root)
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
 
-
         val book = databaseHandler.searchBook(getId())
+
+        binding.invalidNameTextView.isVisible = false
+        binding.invalidAuthorTextView.isVisible = false
+        binding.invalidDescriptionTextView.isVisible = false
 
         binding.bookImageView.setImageBitmap(convertToImage(book.image!!))
         binding.nameEditText.setText(book.name)
@@ -56,7 +66,24 @@ class EditActivity : AppCompatActivity() {
             R.id.done_action -> {
                 if(binding.nameEditText.text.toString() == "" || binding.authorEditText.text.toString() == ""
                     || binding.descriptionEditText.text.toString() == "") {
-                    Toast.makeText(applicationContext, "Please Enter all the fields", Toast.LENGTH_LONG).show()
+
+                    if(binding.nameEditText.text.toString() == "") {
+                        binding.invalidNameTextView.isVisible = true
+                    } else {
+                        binding.invalidNameTextView.isVisible = false
+                    }
+
+                    if(binding.authorEditText.text.toString() == "") {
+                        binding.invalidAuthorTextView.isVisible = true
+                    } else {
+                        binding.invalidAuthorTextView.isVisible = false
+                    }
+
+                    if(binding.descriptionEditText.text.toString() == "") {
+                        binding.invalidDescriptionTextView.isVisible = true
+                    } else {
+                        binding.invalidDescriptionTextView.isVisible = false
+                    }
                 } else {
                     updateBook()
                     finish()
@@ -141,6 +168,23 @@ class EditActivity : AppCompatActivity() {
             imageUri = it.data?.data
             binding.bookImageView.setImageBitmap(it.data!!.extras?.get("data") as Bitmap)
         }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    Log.d("focus", "touchevent")
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 
 }
--- app/src/main/java/com/example/bookstoreapp/MainActivity.kt
+++ app/src/main/java/com/example/bookstoreapp/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.bookstoreapp
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.isVisible
 import com.example.bookstoreapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +22,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.invalidUsernameTextView.isVisible = false
+        binding.invalidPasswordTextView.isVisible = false
         binding.signInButton.setOnClickListener { launchList() }
     }
 
@@ -29,8 +40,32 @@ class MainActivity : AppCompatActivity() {
             startActivity(listIntent)
             binding.usernameEditText.setText("")
             binding.passwordEditText.setText("")
+            binding.invalidUsernameTextView.isVisible = false
+            binding.invalidPasswordTextView.isVisible = false
         } else {
-            Toast.makeText(applicationContext,"Invalid User ID or Password",Toast.LENGTH_SHORT).show()
+            if(userId == "" || userId != "SS") {
+                binding.invalidUsernameTextView.isVisible = true
+            }
+            if(password == "" || password != "11111") {
+                binding.invalidPasswordTextView.isVisible = true
+            }
         }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    Log.d("focus", "touchevent")
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
